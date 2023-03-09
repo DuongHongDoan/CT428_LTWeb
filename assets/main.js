1aa9b4a980c3f6db604913399a6ec272111d981d
Doan <duonghongdoan.hp2020@gmail.com>
2023-03-09 21:56:45 +0700

    update animation

--- assets/main.js
+++ assets/main.js
@@ -2,6 +2,9 @@
 const menu = document.querySelector(".navbar-links")
 const menuButton = document.querySelector(".navbar-icons")
 const overlay = document.querySelector("#overlay")
+const overlaySearch = document.querySelector('#overlay-search')
+const search = document.querySelector(".input-group-collap")
+const searchButton = document.querySelector(".search-btn")
 
 menuButton.addEventListener('click', ()=> { 
     menu.classList.toggle("navbar-open"); 
@@ -14,3 +17,22 @@ overlay.addEventListener('click', () => {
     menuButton.classList.toggle("icon-back");
     overlay.classList.toggle("show");
 });
+
+searchButton.addEventListener('click', ()=> {
+    search.classList.toggle("search-open");
+    searchButton.classList.toggle("cancle-btn");
+    overlaySearch.classList.toggle("show-search");
+});
+
+overlaySearch.addEventListener('click', () => {
+    search.classList.toggle("search-open");
+    searchButton.classList.toggle("cancle-btn");
+    overlaySearch.classList.toggle("show-search");
+});
+function on() {
+    document.getElementById("overlay-search").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay-search").style.display = "none";
+  }
