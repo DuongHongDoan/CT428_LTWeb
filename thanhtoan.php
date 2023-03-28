8c478651d1ac070517907926c7d6151b110ee385
Maitanvo <vob2013574@student.ctu.edu.vn>
2023-03-28 19:50:25 +0700

    Maitanvo_thanhtoan_moisuaphpthoikhongcosuajkhac

---
+++ thanhtoan.php
@@ -0,0 +1,389 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!-- Nhung gg font -> Start -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap"
+      rel="stylesheet"
+    />
+    <!-- Nhung gg font -> End -->
+    <link
+      rel="stylesheet"
+      href="./assets/fontawesome-free-6.0.0-web/css/all.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="./assets/fontawesome-free-6.0.0-web/js/all.min.js"
+    />
+    <link
+      rel="stylesheet"
+      href="./assets/bootstrap-5.1.3-dist/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="./assets/owlcarousel/assets/owl.carousel.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="./assets/owlcarousel/assets/owl.theme.default.min.css"
+    />
+    <link rel="stylesheet" href="./assets/main.css" />
+    <title>Thanh toán</title>
+  </head>
+  <body>
+    <!-- overlay -->
+    <section id="overlay"></section>
+    <!-- Phan dau -->
+    <header>
+      <marquee
+        class="scroll text"
+        bgcolor="#2E8B57"
+        style="color: #fff"
+        behavior=""
+        direction=""
+      >
+        Cửa hàng Kpop
+      </marquee>
+      <!-- logo, search, cart, account -->
+      <div class="container">
+        <div class="row">
+          <nav class="navbar navbar-expand-sm navbar-dark navbar-center">
+            <div class="col-5">
+              <div class="container-fluid">
+                <a class="navbar-brand" href="./index.html">
+                  <img
+                    src="./img/logo.png"
+                    alt="Avatar Logo"
+                    class="rounded-pill"
+                  />
+                </a>
+              </div>
+            </div>
+            <div class="col">
+              <section id="overlay-search">
+                <div class="input-group-collap">
+                  <i class="close-btn fa-solid fa-xmark"></i>
+                  <div class="collap mb-3">
+                    <input
+                      type="text"
+                      class="input-collap"
+                      placeholder="Search"
+                    />
+                    <button>
+                      <i class="fas fa-search"></i>
+                    </button>
+                  </div>
+                </div>
+              </section>
+
+              <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Search" />
+                <button class="btn-search search-btn" type="submit">
+                  <i class="header__search-btn-icon fas fa-search"></i>
+                </button>
+              </div>
+            </div>
+            <div class="col-3 icons">
+              <a href="#">
+                <i class="icon-cart fa-solid fa-cart-shopping"></i>
+              </a>
+              <a href="#">
+                <i class="icon-user fa-regular fa-user ps-2"></i>
+              </a>
+            </div>
+          </nav>
+        </div>
+      </div>
+      <!-- Thanh dieu huong (navbar) -->
+      <div
+        style="background-color: #2e8b57"
+        class="container-fluid navbar-init"
+      >
+        <nav class="navbar navbar-expand-sm navbar-light">
+          <div class="container">
+            <ul class="navbar-nav navbar-links">
+              <li class="nav-item">
+                <a class="nav-link" href="./index.html">Trang chủ</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="./intro.html">Giới thiệu</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Liên hệ</a>
+              </li>
+            </ul>
+            <!-- nut gom menu -->
+            <div class="navbar-icons">
+              <div class="navbar-icon"></div>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- Phan than -->
+    <div class="row justify-content-center" style="margin: 20px 0">
+      <div class="col-sm-7">
+        <h1>Thanh Toán</h1>
+        <div class="mb-3 mt-3">
+          <label for="email" class="form-label"
+            ><h4>Thông tin cá nhân:</h4></label
+          >
+          <label for="email" class="form-label"
+            >Đã có tài khoản: <a href="#">Đăng Nhập</a></label
+          >
+          <div class="form-floating mb-3 mt-3">
+            <input
+              type="text"
+              class="form-control"
+              id="email"
+              placeholder="Enter email"
+              name="email"
+            />
+            <label for="email">Email</label>
+          </div>
+          <div class="form-check">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              id="check1"
+              name="option1"
+              value="something"
+              checked
+            />
+            <label class="form-check-label">Gửi thông tin giao hàng</label>
+          </div>
+        </div>
+        <div>
+          <label for="sel1"><h4>Địa chỉ giao hàng:</h4></label>
+          <div class="form-floating">
+            <select class="form-select" id="sel1" name="sellist">
+              <option>TP HCM</option>
+              <option>Huế</option>
+              <option>Hà Nội</option>
+              <option>Các tỉnh khác</option>
+            </select>
+            <label for="sel1" class="form-label">Chọn thành phố bạn ở:</label>
+          </div>
+          <div class="form-floating mb-3 mt-3">
+            <input
+              type="text"
+              class="form-control"
+              id="diachi"
+              placeholder="Nhập địa chỉ cụ thể"
+              name="diachi"
+            />
+            <label for="diachi">Địa chỉ cụ thể</label>
+          </div>
+          <div class="form-floating mb-3 mt-3">
+            <input
+              type="text"
+              class="form-control"
+              id="hoten"
+              placeholder="Nhập Họ và Tên"
+              name="hoten"
+            />
+            <label for="hoten">Họ và tên</label>
+          </div>
+          <div class="form-floating mb-3 mt-3">
+            <input
+              type="number"
+              class="form-control"
+              id="sdt"
+              placeholder="Nhập số điện thoại"
+              name="sdt"
+            />
+            <label for="sdt">Số Điện thoại của bạn</label>
+          </div>
+          <div class="form-check form-switch" style="margin-top: 10px">
+            <input
+              class="form-check-input"
+              type="checkbox"
+              id="mySwitch"
+              name="darkmode"
+              value="yes"
+              checked
+            />
+            <label class="form-check-label" for="mySwitch">Lưu thông tin</label>
+          </div>
+        </div>
+        <div>
+          <a
+            href="thanhtoantc.html"
+            class="btn btn-success"
+            style="margin-top: 10px"
+            >Đặt Hàng</a
+          >
+        </div>
+      </div>
+      <div class="col-sm-5" style="background-color: #f7f4f4">
+        <div class="mathang">
+          <h1>mat hang</h1>
+        </div>
+        <div class="magg">
+          <div class="form-floating mb-3 mt-3">
+            <input
+              type="text"
+              class="form-control"
+              id="magg"
+              placeholder="Nhập mã giảm giá"
+              name="magg"
+            />
+            <label for="magg">Mã giảm giá</label>
+          </div>
+          <a
+            href="thanhtoantc.html"
+            class="btn btn-success"
+            style="margin-top: 10px"
+            >Nhập mã giảm giá</a
+          >
+        </div>
+        <div class="thue">
+          
+        </div>
+        <div class="tonggia">
+
+        </div>
+      </div>
+    </div>
+    <!-- Phan chan -->
+    <footer
+      class="text-center text-lg-start bg-opacity-100 text-muted"
+      style="margin-top: 50px"
+    >
+      <section
+        class="footertext"
+        style="padding-top: 5px; background-color: #2e8b57"
+      >
+        <div class="container text-center text-md-start mt-5">
+          <div class="row row-cols-3mt-3">
+            <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 mx-auto mb-4">
+              <h6 class="text-uppercase fw-bold mb-4">CHĂM SÓC KHÁCH HÀNG</h6>
+              <p>
+                <a href="#!" class="text-reset">Trung tâm hỗ trợ</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Hướng dẫn mua hàng</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Chăm sóc khách hàng</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Vận chuyển</a>
+              </p>
+            </div>
+            <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 mx-auto mb-4">
+              <h6 class="text-uppercase fw-bold mb-4">CHÍNH SÁCH SHOP</h6>
+              <p>
+                <a href="#!" class="text-reset">Điều khoản</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Chính sách bảo mật</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Tuyển Dụng</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Liên kế tiếp thị</a>
+              </p>
+            </div>
+            <div
+              class="col-sm-4 col-md-4 col-lg-4 col-xl-4 mx-auto mb-md-0 mb-4"
+            >
+              <h6 class="text-uppercase fw-bold mb-4">
+                <span>Get connected with us on social networks:</span>
+              </h6>
+              <div class="icon">
+                <a class="footertext" href="" class="me-4 link-secondary">
+                  <i style="padding-left: 0px" class="fab fa-facebook-f"></i>
+                </a>
+                <a class="footertext" href="" class="me-4 link-secondary">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a class="footertext" href="" class="me-4 link-secondary">
+                  <i class="fab fa-google"></i>
+                </a>
+                <a class="footertext" href="" class="me-4 link-secondary">
+                  <i class="fab fa-instagram"></i>
+                </a>
+                <a class="footertext" href="" class="me-4 link-secondary">
+                  <i class="fab fa-linkedin"></i>
+                </a>
+                <a class="footertext" href="" class="me-4 link-secondary">
+                  <i class="fab fa-github"></i>
+                </a>
+              </div>
+              <br />
+              <p class="footertext">
+                <i class="fas fa-phone"></i> + 01 234 567 88
+              </p>
+              <p class="footertext">
+                <i class="fas fa-print"></i> + 01 234 567 89
+              </p>
+            </div>
+          </div>
+          <div class="row row-cols-2">
+            <div class="col-sm-4 col-md-4 col-lg-4 col-xl-4 mx-auto mb-4">
+              <h6 class="text-uppercase fw-bold mb-4">THANH TOÁN</h6>
+              <p>
+                <a href="#!" class="text-reset">Pricing</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Settings</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Orders</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Help</a>
+              </p>
+            </div>
+            <div class="col-sm-8 col-md-8 col-lg-8 col-xl-8 mx-auto mb-4">
+              <h6 class="text-uppercase fw-bold mb-4">VẬN CHUYỂN</h6>
+              <p>
+                <a href="#!" class="text-reset">Pricing</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Settings</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Orders</a>
+              </p>
+              <p>
+                <a href="#!" class="text-reset">Help</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div class="text-center p-4 footertext" style="background-color: #1a5534">
+        © 2023 Copyright:
+        <a class="text-reset fw-bold footertext" href="https://mdbootstrap.com/"
+          >Kpopstore.com</a
+        >
+      </div>
+    </footer>
+    <!-- Thanh toan, van chuyen -->
+
+    <!-- Nhung cac script -->
+    <script src="./assets/main.js"></script>
+    <script src="./assets/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/jquery-3.6.0.min.js"></script>
+    <script src="./assets/owlcarousel/owl.carousel.min.js"></script>
+    <script>
+      var owl = $(".owl-carousel");
+      owl.owlCarousel({
+        items: 4,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+      });
+    </script>
+  </body>
+</html>
