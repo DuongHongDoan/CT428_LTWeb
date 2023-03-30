a3ed321f9a615259646cf10d4cf8e38d261b5b64
Doan <duonghongdoan.hp2020@gmail.com>
2023-03-30 07:31:05 +0700

    test

--- pages/products/giohang.php
+++ pages/products/giohang.php
@@ -1,1 +1,16 @@
-<p>gio hang</p>
+<!-- <p>gio hang</p> -->
+<?php
+    session_start();
+    if(isset($_SESSION['cart'])) {
+        print_r($_SESSION['cart']);
+    }
+
+?>
+
+<h1>Gio hang</h1>
+
+<?php
+    if(isset($_SESSION['cart'])) {
+        print_r($_SESSION['cart']);
+    }
+?>
