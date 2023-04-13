878f10db2b1c3c0c30d4959cf3464eea8c98af38
BachToanMy <126689705+BachToanMy@users.noreply.github.com>
2023-04-13 23:04:56 +0700

    BTM ne

--- pages/products/hoadon.php
+++ pages/products/hoadon.php
@@ -1,2 +1,2 @@
 
-<p>Cám ơn bạn đã mua hàng ,chúng tôi sẽ liên hệ bạn trong thời gian sớm nhất</p>
+<p style="text-align:center"><h2>Cám ơn bạn đã mua hàng ,chúng tôi sẽ liên hệ bạn trong thời gian sớm nhất</h2></p>
