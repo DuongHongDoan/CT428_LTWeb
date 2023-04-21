448d937e4ba6804c748140317aa12f1437c6af6c
Maitanvo <vob2013574@student.ctu.edu.vn>
2023-04-21 21:18:23 +0700

    maitanvo

--- pages/products/vanchuyen.php
+++ pages/products/vanchuyen.php
@@ -7,29 +7,6 @@ if (isset($_SESSION['id_khachhang'])) {
 	<title>Vận chuyển</title>
 </head>
 <h4>Thông tin vận chuyển</h4>
-<?php
-if (isset($_POST['themvanchuyen'])) {
-	$ten = $_POST['name'];
-	$sdt = $_POST['phone'];
-	$diachi = $_POST['address'];
-	$chuthich = $_POST['note'];
-	$id_khachhang = $_SESSION['id_khachhang'];
-	$sql_them_vanchuyen = mysqli_query($conn, "INSERT INTO tbl_vanchuyen(ten,sodienthoai,diachi,chuthich,id_khachhang) VALUES('$ten','$sdt','$diachi','$chuthich','$id_khachhang')");
-	if ($sql_them_vanchuyen) {
-		echo '<script>alert("Thêm địa chỉ thành công")</script>';
-	}
-} elseif (isset($_POST['capnhatvanchuyen'])) {
-	$ten = $_POST['name'];
-	$sdt = $_POST['phone'];
-	$diachi = $_POST['address'];
-	$chuthich = $_POST['note'];
-	$id_khachhang = $_SESSION['id_khachhang'];
-	$sql_update_vanchuyen = mysqli_query($conn, "UPDATE tbl_vanchuyen SET ten='$ten',sodienthoai='$sdt',diachi='$diachi',chuthich='$chuthich',id_khachhang='$id_khachhang' WHERE id_khachhang='$id_khachhang'");
-	if ($sql_update_vanchuyen) {
-		echo '<script>alert("Cập nhật địa chỉ thành công")</script>';
-	}
-}
-?>
 <div class="row">
 	<?php
 	$id_khachhang = $_SESSION['id_khachhang'];
@@ -50,49 +27,93 @@ if (isset($_POST['themvanchuyen'])) {
 	}
 	?>
 	<div class="col-md-12">
-		<form action="" autocomplete="off" method="POST" id="thongtinthanhtoan" name="thongtinthanhtoan">
-			<div class="form-group">
-				<label for="email">Họ và tên</label>
-				<input type="text" name="name" class="form-control" value="<?php echo $ten ?>" placeholder="Nhập Họ Tên">
-			</div>
-			<div class="form-group">
-				<label for="email">Phone</label>
-				<input type="number" name="phone" class="form-control" value="<?php echo $sdt ?>" placeholder="Nhập số điện thoại của bạn">
-			</div>
-			<div class="form-group">
-				<label for="email">Địa chỉ</label>
-				<input type="text" name="address" class="form-control" value="<?php echo $diachi ?>" placeholder="Nhập địa chỉ cần giao hàng">
-			</div>
-			<div class="form-group">
-				<label for="email">Ghi chú</label>
-				<input type="text" name="note" class="form-control" value="<?php echo $chuthich ?>" placeholder="Chú thích">
-			</div>
-			<?php
-			if ($ten == '' && $sdt == '') {
-			?>
-				<button type="submit" name="themvanchuyen" class="btn btn-primary">Thêm vận chuyển</button>
-			<?php
-			} elseif ($ten != '' || $sdt != '') {
-			?>
-				<button type="submit" name="capnhatvanchuyen" class="btn btn-success">Cập nhật vận chuyển</button>
-			<?php
-			}
-			?>
+		<ul>
+			<li>Họ và tên vận chuyển : <b><?php echo $ten ?></b></li>
+			<li>Số điện thoại : <b><?php echo $sdt ?></b></li>
+			<li>Địa chỉ : <b><?php echo $diachi ?></b></li>
+			<li>Ghi chú : <b><?php echo $chuthich ?></b></li>
+		</ul>
+		<div class="modal" id="myModal">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h4 class="modal-title">Cập nhật thông tin vận chuyển</h4>
+						<button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+					</div>
+					<div class="modal-body">
+						<?php
+						if (isset($_POST['themvanchuyen'])) {
+							$ten = $_POST['name'];
+							$sdt = $_POST['phone'];
+							$diachi = $_POST['address'];
+							$chuthich = $_POST['note'];
+							$id_khachhang = $_SESSION['id_khachhang'];
+							$sql_them_vanchuyen = mysqli_query($conn, "INSERT INTO tbl_vanchuyen(ten,sodienthoai,diachi,chuthich,id_khachhang) VALUES('$ten','$sdt','$diachi','$chuthich','$id_khachhang')");
+							if ($sql_them_vanchuyen) {
+								echo '<script>alert("Thêm địa chỉ thành công")</script>';
+							}
+						} elseif (isset($_POST['capnhatvanchuyen'])) {
+							$ten = $_POST['name'];
+							$sdt = $_POST['phone'];
+							$diachi = $_POST['address'];
+							$chuthich = $_POST['note'];
+							$id_khachhang = $_SESSION['id_khachhang'];
+							$sql_update_vanchuyen = mysqli_query($conn, "UPDATE tbl_vanchuyen SET ten='$ten',sodienthoai='$sdt',diachi='$diachi',chuthich='$chuthich',id_khachhang='$id_khachhang' WHERE id_khachhang='$id_khachhang'");
+							if ($sql_update_vanchuyen) {
+								echo '<script>alert("Cập nhật địa chỉ thành công")</script>';
+							}
+						}
+						?>
+						<form action="" autocomplete="off" method="POST" id="thongtinthanhtoan" name="thongtinthanhtoan">
+							<div class="form-group">
+								<label for="email">Họ và tên</label>
+								<input type="text" name="name" class="form-control" value="<?php echo $ten ?>" placeholder="Nhập Họ Tên">
+							</div>
+							<div class="form-group">
+								<label for="email">Phone</label>
+								<input type="number" name="phone" class="form-control" value="<?php echo $sdt ?>" placeholder="Nhập số điện thoại của bạn">
+							</div>
+							<div class="form-group">
+								<label for="email">Địa chỉ</label>
+								<input type="text" name="address" class="form-control" value="<?php echo $diachi ?>" placeholder="Nhập địa chỉ cần giao hàng">
+							</div>
+							<div class="form-group">
+								<label for="email">Ghi chú</label>
+								<input type="text" name="note" class="form-control" value="<?php echo $chuthich ?>" placeholder="Chú thích">
+							</div>
+							<?php
+							if ($ten == '' && $sdt == '') {
+							?>
+								<button type="submit" name="themvanchuyen" class="btn btn-primary">Thêm vận chuyển</button>
+							<?php
+							} elseif ($ten != '' || $sdt != '') {
+							?>
+								<button type="submit" name="capnhatvanchuyen" class="btn btn-success">Cập nhật vận chuyển</button>
+							<?php
+							}
+							?>
 
-			<?php
-			if (isset($_POST['thongtinthanhtoan'])) {
-				$name = $_POST['name'];
-				$diachi = $_POST['address'];
-				$sdt = $_POST['phone'];
-				if ($name == '' || $diachi == '' || $sdt == '') {
-					echo '<script>alert("Hãy nhập thông tin đi!!")</script>';
-				} else {
-					echo '<script>location.href = "index.php?quanly=thanhtoan";</script>';
-				}
-			}
-			?>
-		</form>
+							<?php
+							if (isset($_POST['thongtinthanhtoan'])) {
+								if ($ten == '' || $diachi == '' || $sdt == '') {
+									echo '<script>alert("Hãy nhập thông tin đi!!")</script>';
+								} else {
+									echo '<script>location.href = "index.php?quanly=thanhtoan";</script>';
+								}
+							}
+							?>
+						</form>
+					</div>
+
+					<div class="modal-footer">
+						<button type="button" class="btn btn-danger" data-bs-dismiss="modal">Đóng</button>
+					</div>
+
+				</div>
+			</div>
+		</div>
 	</div>
+	<h3>Thông tin giỏ hàng</h3>
 	<table style="width:100%;text-align: center;border-collapse: collapse;" border="1">
 		<tr>
 			<th>Id</th>
@@ -148,7 +169,13 @@ if (isset($_POST['themvanchuyen'])) {
 					<?php
 					if ($_SESSION['id_khachhang'] != -1) {
 					?>
-						<p><button type="submit" name="thongtinthanhtoan" form="thongtinthanhtoan" class="btn btn-success">Thanh Toán</button></p>
+
+						<p>
+							<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">
+								Thêm địa chỉ giao hàng
+							</button>
+							<button type="submit" name="thongtinthanhtoan" form="thongtinthanhtoan" class="btn btn-success">Thanh Toán</button>
+						</p>
 					<?php
 					} else {
 					?>
