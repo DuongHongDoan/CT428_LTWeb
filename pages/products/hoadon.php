757b193d0caeefcb9cbeb61591ae42ea988a3529
Maitanvo <vob2013574@student.ctu.edu.vn>
2023-04-22 13:09:37 +0700

    maitanvo

--- pages/products/hoadon.php
+++ pages/products/hoadon.php
@@ -2,39 +2,106 @@
     <title>Hóa đơn</title>
 </head>
 <?php
-$id_dangky = $_SESSION['id_khachhang'];
-$sql_get_donhang = mysqli_query($conn, "SELECT * FROM tbl_donhang WHERE id_khachhang='$id_dangky' LIMIT 1");
-$count = mysqli_num_rows($sql_get_donhang);
-if ($count > 0) {
-    $row_get_donhang = mysqli_fetch_array($sql_get_donhang);
-    $madh = $row_get_donhang['ma_donhang'];
-    $tt = $row_get_donhang['trangthai_donhang'];
-    $nl = $row_get_donhang['ngaylap_donhang'];
-    $pt = $row_get_donhang['pt_thanhtoan'];
-    $dcgh = $row_get_donhang['vanchuyen_donhang'];
-} else {
-}
-foreach ($_SESSION['donhang'] as $donhang) {
-    $donhangm[] = array('tensp'=>$donhang['tensp'], 'id'=> $cart_item['id'], 'soluong'=>$cart_item['soluong'],'giasp'=> $cart_item['giasp'], 'hinhanh'=>$cart_item['hinhanh'] );
-}
+echo '<script>alert("Cảm ơn vì đã đặt hàng")</script>'
 ?>
-<table style="width:100%;text-align: center;border-collapse: collapse;" border="1">
+<table style="width:100%;text-align: center;border-collapse: collapse;" border="1" class="table table-hover">
     <tr>
-        <td>
+        <th>
+            Mã Đơn hàng
+        </th>
+        <th>
+            Trạng thái
+        </th>
+        <th>
+            Sản phẩm
+        </th>
+        <th>
+            Số lượng
+        </th>
+        <th>
+            Ngày lập
+        </th>
+        <th>
+            Thanh toán
+        </th>
+        <th>
+            Địa chỉ
+        </th>
+        <th>
+            Tổng tiền
+        </th>
+    </tr>
+    <?php
+    $id_dangky = $_SESSION['id_khachhang'];
+    $sql_get_donhang = mysqli_query($conn, "SELECT * FROM tbl_donhang WHERE id_khachhang='$id_dangky'");
+
+    $sql_get_vanchuyen = mysqli_query($conn, "SELECT * FROM tbl_vanchuyen WHERE id_khachhang='$id_dangky' LIMIT 1");
+    $row_get_vanchuyen = mysqli_fetch_array($sql_get_vanchuyen);
+    $dc = $row_get_vanchuyen['diachi'];
+    for ($x = 1; $x <= $count; $x++) {
+
+        $count = mysqli_num_rows($sql_get_donhang);
+        if ($count > 0) {
+            $row_get_donhang = mysqli_fetch_array($sql_get_donhang);
+            $madh = $row_get_donhang['ma_donhang'];
+            $tt = $row_get_donhang['trangthai_donhang'];
+            $nl = $row_get_donhang['ngaylap_donhang'];
+            $pt = $row_get_donhang['pt_thanhtoan'];
+            $dcgh = $row_get_donhang['vanchuyen_donhang'];
+            (float)$tongtien = $row_get_donhang['tongtien'];
+        }
+    ?>
+
+        <tr>
+            <td>
+                <?php echo $madh; ?>
+
+            </td>
+            <td>
+                <?php echo $tt; ?>
+            </td>
+            <td>
+                <?php
+                $sql_get_donhang_ct = mysqli_query($conn, "SELECT * FROM tbl_donhang_chitiet WHERE ma_donhang='$madh'");
+                $count1 = mysqli_num_rows($sql_get_donhang_ct);
+                if ($count1 > 0) {
+                    for ($i = 1; $i <= $count1; $i++) {
+                        $row_get_donhang_ct = mysqli_fetch_array($sql_get_donhang_ct);
+                        $sp = $row_get_donhang_ct['id_sanpham'];
+                        $sql_get_sanpham = mysqli_query($conn, "SELECT * FROM tbl_products WHERE id_sanpham='$sp'");
+                        $row_get_sanpham = mysqli_fetch_array($sql_get_sanpham);
+                        $tensp = $row_get_sanpham['tensp'];
+                        echo $tensp . '</br>';
+                    }
+                ?>
+            </td>
+            <td>
             <?php
-            echo $madh;
-            echo $tt;
-            echo $nl;
-            echo $pt;
+                    $sql_get_donhang_sl = mysqli_query($conn, "SELECT soluongmua FROM tbl_donhang_chitiet WHERE ma_donhang='$madh'");
+                    $count2 = mysqli_num_rows($sql_get_donhang_sl);
+                    for ($a = 1; $a <= $count2; $a++) {
+                        $row_get_donhang_sl = mysqli_fetch_array($sql_get_donhang_sl);
+                        $sl = $row_get_donhang_sl['soluongmua'];
+                        echo $sl . '</br>';
+                    }
+                }
             ?>
-        </td>
-    </tr>
-    <tr>
-        <th>Id</th>
-        <th>Tên sản phẩm</th>
-        <th>Hình ảnh</th>
-        <th>Số lượng</th>
-        <th>Giá sản phẩm</th>
-        <th>Thành tiền</th>
-    </tr>
+            </td>
+            <td>
+                <?php echo $nl; ?>
+            </td>
+            <td>
+                <?php echo $pt; ?>
+            </td>
+            <td>
+                <?php echo $dc; ?>
+            </td>
+            <td>
+                <?php echo $tongtien . 'đ' ?>
+            </td>
+        </tr>
+    <?php
+    }
+    ?>
+
 </table>
--- pages/products/xulythanhtoan.php
+++ pages/products/xulythanhtoan.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 date_default_timezone_set('Asia/Ho_Chi_Minh');
-$now = date('l j-m-Y G:i:s A');
-
+$now = date("l j/n/Y G:i:s A");
 $conn = new mysqli("localhost", "root", "", "ct428_ltweb");
 // Kiem tra ket noi
 if ($conn->connect_errno) {
@@ -10,9 +9,8 @@ if ($conn->connect_errno) {
 	exit();
 }
 $id_khachhang = $_SESSION['id_khachhang'];
-$ptThanhToan = $_POST['payment'];
-$ttdonhang = 1;
-
+$code_order = rand(0, 9999);
+$cart_payment = $_POST['payment'];
 //lay id thong tin van chuyen
 $sql_get_vanchuyen = mysqli_query($conn, "SELECT * FROM tbl_vanchuyen WHERE id_khachhang='$id_khachhang' LIMIT 1");
 $row_get_vanchuyen = mysqli_fetch_array($sql_get_vanchuyen);
@@ -23,19 +21,15 @@ foreach ($_SESSION['cart'] as $key => $value) {
 	$tongtien += $thanhtien;
 }
 
-
-if ($ptThanhToan == 'tienmat' || $ptThanhToan == 'chuyenkhoan') {
+if ($cart_payment == 'tienmat' || $cart_payment == 'chuyenkhoan') {
 	//insert vào đơn hàng
-	$insert_cart = "INSERT INTO tbl_donhang(id_khachhang,trangthai_donhang,ngaylap_donhang,pt_thanhtoan,vanchuyen_donhang) VALUE('" . $id_khachhang . "','" . $ttdonhang . "','" . $now . "','" . $ptThanhToan . "','" . $id_shipping . "')";
+	$insert_cart = "INSERT INTO tbl_donhang(id_khachhang,trangthai_donhang,ngaylap_donhang,pt_thanhtoan,vanchuyen_donhang,ma_donhang,tongtien) VALUE('" . $id_khachhang . "','Đang chờ xác nhận','" . $now . "','" . $cart_payment . "','" . $id_shipping . "','" . $code_order . "','" . $tongtien . "')";
 	$cart_query = mysqli_query($conn, $insert_cart);
 	//them don hàng chi tiet
-	$sql_get_donhang = mysqli_query($conn, "SELECT * FROM tbl_donhang WHERE id_khachhang='$id_khachhang' LIMIT 1");
-	$row_get_vanchuyen = mysqli_fetch_array($sql_get_donhang);
-	$id_donhang = $row_get_vanchuyen['id_donhang'];
 	foreach ($_SESSION['cart'] as $key => $value) {
 		$id_sanpham = $value['id'];
 		$soluong = $value['soluong'];
-		$insert_order_details = "INSERT INTO tbl_donhang_chitiet(id_sanpham,id_donhang,soluongmua) VALUE('" . $id_sanpham . "','" . $id_donhang . "','" . $soluong . "')";
+		$insert_order_details = "INSERT INTO tbl_donhang_chitiet(id_sanpham,ma_donhang,soluongmua) VALUE('" . $id_sanpham . "','" . $code_order . "','" . $soluong . "')";
 		mysqli_query($conn, $insert_order_details);
 	}
 
